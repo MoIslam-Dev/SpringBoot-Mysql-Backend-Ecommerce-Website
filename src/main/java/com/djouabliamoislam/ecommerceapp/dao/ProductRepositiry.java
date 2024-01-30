cef3184b7247369b3351cd0ca781ffc25ecfad4b
Djouablia MoIslam <Mohamed.djouablia@univ-constantine2.dz>
2024-01-30 14:28:51 +0100

    Update project structure

--- src/main/java/com/djouabliamoislam/ecommerceapp/dao/ProductRepositiry.java
+++ src/main/java/com/djouabliamoislam/ecommerceapp/dao/ProductRepositiry.java
@@ -1,2 +1,15 @@
-package com.djouabliamoislam.ecommerceapp.dao;public interface ProductRepositiry {
+package com.djouabliamoislam.ecommerceapp.dao;
+
+import com.djouabliamoislam.ecommerceapp.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepositiry extends JpaRepository<Product,Long> {
+
+    Page<Product> findByCategoryId(@Param("id") Long id ,Pageable pageable);
+    Page<Product> findByNameContaining(@Param("name") String name ,Pageable pageable);
 }
